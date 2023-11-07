c8a9025e14ce6d48ce9f0544ccb9bde4428a2c27
qiye <1710797241@qq.com>
2023-11-07 15:54:04 +0800

    chore: init

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import ModelEditPanel from './ModelEditPanel/'
+import ModelChoose from './ModelChoose/'
+
+export { ModelEditPanel ,ModelChoose}
---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    //当前模型
+    modelApi: {},
+    //当前材质
+    selectMesh:{}
+  },
+  getters: {
+    selectMeshUuid:(state)=>state.selectMesh.uuid
+  },
+  mutations: {
+    SET_MODEL_API: (state, modelApi) => {
+      state.modelApi = modelApi
+    },
+    SELECT_MESH: (state, selectMesh) => {
+      state.selectMesh = selectMesh
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
